77e4501e2df730e761e88a8ba811b198fb501c49
Aitorsg98 <49958246+Aitorsg98@users.noreply.github.com>
2020-01-16 17:44:58 +0100

    1.1

--- EscriuFitxerAleatori.java
+++ EscriuFitxerAleatori.java
@@ -4,37 +4,41 @@ import java.io.*;
 
 public class EscriuFitxerAleatori {
 	public static void main(String[] args) throws IOException {
-		File fitxer = new File("/home/user/Escriptori/llibres");
-		//Crea un flux (stream) d'arxiu d'accés aleatori per llegir
+		File fitxer = new File("persones.txt");
+		//Crea un flux (stream) d'arxiu d'accÃ©s aleatori per llegir
 		RandomAccessFile aleatoriFile = new RandomAccessFile(fitxer, "rw");
 		//Les dades per inserir
-		String titol[] = {"El capital al segle xxi", "Sàpiens. una breu història de la humanitat", "És l'hora dels 					adéus?", "Els jueus catalans", "Raó i emoció"};
-		int isbn[] = {190249335, 190444337, 543219331, 455249337, 780249335};
-		String autor[] = {"Piketty, thomas", "Yuval noah harari", "Sala i martin, xavier", "Forcano, manuel", 						"Salmurri trinxet, ferran"};
-		String editorial[] = {"MAGRANA EDICIONS DE LA", "EDICIONS 62 PENINSULA", "ROSA DELS VENTS", "angle 								editorial", "MAGRANA EDICIONS DE LA"};
-		double preu[] = {25, 23.9, 16.9, 18.9, 12};
-		//Construeix un buffer (memòria intermèdia) de strings
+		String nom[] = {"Nom1", "Nom2", "Nom3", "Nom4", "Nom5"};
+		String cognoms[] = {"Cognom1.1 Cognom1.2", "Cognom2.1 Cognom2.2", "Cognom3.1 Cognom3.2", "Cognom4.1 Cognom4.2", "Cognom5.1 Cognom5.2"};
+		String edat[] = {"20", "21", "22", "23", "24"};
+		String alÃ§ada[] = {"1.68", "1.69", "1.70", "1.71", "1.72"};
+		String pes[] = {"70", "71", "72", "73", "74"};
+		//Construeix un buffer (memÃ²ria intermÃ¨dia) de strings
 		StringBuffer buffer = null;
 		
-		for (int i=0; i<titol.length; i++) {
+		for (int i=0; i<nom.length; i++) {
 			aleatoriFile.writeInt(i+1);//1 enter ocupa 4 bytes
-			//50 caràcters a 2bytes/caràcter 100 bytes
-			buffer = new StringBuffer (titol[i]);
-			buffer.setLength(50);
+			//20 carÃ cters a 2bytes/carÃ cter 40 bytes
+			buffer = new StringBuffer (nom[i]);
+			buffer.setLength(20);
 			aleatoriFile.writeChars(buffer.toString());
-			//1 enter ocupa 4 bytes
-			aleatoriFile.writeInt(isbn[i]);
-			//25 caràcters a 2bytes/caràcter 50 bytes
-			buffer = new StringBuffer (autor[i]);
-			buffer.setLength(25);
-			aleatoriFile.writeChars(buffer.toString());
-			//30 caràcters a 2bytes/caràcter 60 bytes
-			buffer = new StringBuffer (editorial[i]);
+			//30 carÃ cters a 2bytes/carÃ cter 60 bytes
+			buffer = new StringBuffer (cognoms[i]);
 			buffer.setLength(30);
 			aleatoriFile.writeChars(buffer.toString());
-			//1 float ocupa 4 bytes
-			aleatoriFile.writeFloat((float)preu[i]);
-			//Total 222 bytes
+			//3 carÃ cters a 2bytes/carÃ cter 6 bytes
+			buffer = new StringBuffer (edat[i]);
+			buffer.setLength(3);
+			aleatoriFile.writeChars(buffer.toString());
+			//4 carÃ cters a 2bytes/carÃ cter 8 bytes
+			buffer = new StringBuffer (alÃ§ada[i]);
+			buffer.setLength(4);
+			aleatoriFile.writeChars(buffer.toString());
+			//4 carÃ cters a 2bytes/carÃ cter 8 bytes
+			buffer = new StringBuffer (pes[i]);
+			buffer.setLength(4);
+			aleatoriFile.writeChars(buffer.toString());
+			//Total 126 bytes
 		}
 		aleatoriFile.close();
 	}
